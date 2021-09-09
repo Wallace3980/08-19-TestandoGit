559d0eb47deb5d640692104f74774759e8509d61
Wallace Rodrigues <aleksander.rodrigues@estudante.ifms.edu.br>
2021-09-09 11:15:53 -0300

    incluindo as imagens no header

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hello word</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require("./src/assets/arrow-left.png")}></Image>
+                <Image source={require("./src/assets/pesquisar.png")}></Image>
+                <Image source={require("./src/assets/compartilhar.png")}></Image>
+                <Text>tanto faz</Text>
+            </View>
+            <Text>hello word</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container:{
+    },
+    header:{
+        minHeight: 93,
+        backgroundColor: "#2E4877",
+        paddingTop: 40,
+    }
 });
