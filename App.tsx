318ed4694044e7f3e24ed9bbfb79fff3c8d7a034
Wallace Rodrigues <aleksander.rodrigues@estudante.ifms.edu.br>
2021-09-09 14:59:54 -0300

    Meu primerio comite

--- App.tsx
+++ App.tsx
@@ -1,17 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+
+const LinksImportantes = ()=>{
+    return(
+        <View>
+            <Text>Meu primeiro componente :D</Text>
+        </View>
+    );
+};
 
 export default function App() {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Image source={require("./src/assets/arrow-left.png")}></Image>
-                <Image source={require("./src/assets/pesquisar.png")}></Image>
-                <Image source={require("./src/assets/compartilhar.png")}></Image>
-                <Text>tanto faz</Text>
+                <Image source={
+                    require("./src/assets/seta.png")}>
+                </Image>
+                <View style={styles.inputTextView}>
+                    <Image source={
+                        require("./src/assets/pesquisar.png")}>
+                    </Image>
+                    <TextInput 
+                        placeholder="Search"
+                        placeholderTextColor="#F5FFFF"
+                        style={styles.textInput}>
+                    </TextInput>
+                </View>
+                <Image source={
+                    require("./src/assets/compartilhar.png")}>
+                </Image>
             </View>
-            <Text>hello word</Text>
+            <LinksImportantes />
             <StatusBar style="auto" />
         </View>
     );
@@ -19,10 +39,30 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container:{
+        flex: 1,
     },
     header:{
         minHeight: 93,
-        backgroundColor: "#2E4877",
+        backgroundColor: "#4369B0",
         paddingTop: 40,
-    }
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 30,
+    },
+    inputTextView:{
+        flex: 2,
+        backgroundColor: "#2C4877",
+        height: 40,
+        width: 30,
+        marginHorizontal: 10,
+        alignItems: "center",
+        borderRadius: 28,
+        flexDirection: "row",
+    },
+    textInput:{
+        color: "#F5FFFF",
+        // maxWidth: 170,
+        flex: 1,
+        paddingRight: 20,
+    },
 });
